d9d43dedbf63564f634fd40db194d8b7d5abb3d7
Rohini Sulatycki <rohini_sulatycki@yahoo.com>
2014-09-25 15:12:54 -0400

    Initial
    Initial

---
+++ Assets/Scripts/Editor/PostBuildTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class PostBuildTrigger
+{
+
+	[PostProcessBuild] // <- this is where the magic happens
+	public static void OnPostProcessBuild(BuildTarget target, string path)
+	{
+		Debug.Log("Post Processing Build 1");
+		
+	}
+	
+	[PostProcessBuild(0)] // <- this is where the magic happens
+	public static void OnPostProcessBuildFirst(BuildTarget target, string path)
+	{
+		Debug.Log("Post Process Build 0");
+	}
+	
+}
+
