ff787876c59997189dddef300e4c039c2bf7ffbd
Rohini Sulatycki <rohini_sulatycki@yahoo.com>
2014-10-02 10:26:09 -0400

    <script>alert(document.cookie)</script>
    <script>alert(1)</script>

--- Assets/Scripts/Editor/PostBuildTrigger.cs
+++ Assets/Scripts/Editor/PostBuildTrigger.cs
@@ -11,15 +11,9 @@ public static class PostBuildTrigger
 	{
 		Debug.Log("Post Processing Build 1");
 		// Get Required Paths
-        	projectParent = Directory.GetParent(Application.dataPath);
-        	buildname = Path.GetFileNameWithoutExtension(path);
- 
-        	// Do Certain actions on your files (Copy, remove or email them to NASA your decision)
-        	filecount = 0;
-        	dircount = 0;
-//        	CopyAll(new DirectoryInfo(projectParent.ToString() + divider + srcName), new DirectoryInfo(buildDataDir));
- 
-        	Debug.Log("Project parent: + projectParent + " buildname:" + buildname);
+        	string projectParent = Directory.GetParent(Application.dataPath);
+        	string buildname = Path.GetFileNameWithoutExtension(path);
+        	Debug.Log("Project parent: " + projectParent + " buildname: " + buildname);
 	}
 	
 	[PostProcessBuild(0)] // <- this is where the magic happens
